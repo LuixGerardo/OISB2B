f506dd7dd3df765c02bffefaf8e0d6c3e8b52ffe
LuixGerardo <100378450+LuixGerardo@users.noreply.github.com>
2022-02-24 14:31:15 -0600

    Agregando
    Agregando

---
+++ OISB2B/OISB2B/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using FreshMvvm;
+using OISB2B.PageModels;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace OISB2B
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            var page = FreshPageModelResolver.ResolvePageModel<LoginPageModel>();
+            var navigationPage = new FreshNavigationContainer(page);
+            MainPage = navigationPage;
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
